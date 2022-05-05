b7a8f6619dbadc900e8fbb6d7e91f27560104fe4
John Efraim Sargado <raimsargado@Johns-Mac-mini.local>
2022-05-05 14:47:49 +0800

    ADD: palette package

--- palette/lib/palette.dart
+++ palette/lib/palette.dart
@@ -1,7 +1,4 @@
 library palette;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'packages/hex_to_color.dart';
+export 'packages/palette_package.dart';
